1139b923d6deea8144666f38dad87713325c91ac
Bob <tzbob@hotmail.com>
2014-01-06 21:29:31 +0100

    rewrite xmonad config

--- home/.xmonad/xmonad.hs
+++ home/.xmonad/xmonad.hs
@@ -17,122 +17,93 @@ import XMonad.Layout.Named
 
 import XMonad.Util.NamedScratchpad
 import XMonad.Util.Run
+import XMonad.Util.Paste
 
 import qualified XMonad.StackSet as W
 import qualified Data.Map as M
 
-myMainColor = "#BCB1B7"
-myBgColor = "#6E839C"
-myTextcolor = "#EFEFEF"
-myLowColor = "#999999"
+data ColorScheme = ColorScheme
+  { foreground :: String
+  , background :: String
+  , empty :: String
+  , hidden :: String
+  , highlight :: String 
+  , seperator :: String }
 
-myNormalBorderColor = myLowColor
-myFocusedBorderColor = myLowColor
+scheme :: ColorScheme
+scheme = ColorScheme
+  { foreground = "#efefef"
+  , background = "#6e839c"
+  , empty = "#666666"
+  , hidden = "#aaaaaa"
+  , highlight = "#efefef" 
+  , seperator = "#bcb1b7" }
 
 -- Key bindings. Add, modify or remove key bindings here.
 -------------------------------------------------------------------------------
+myKeys :: XConfig l -> M.Map (KeyMask, KeySym) (X ())
 myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
+  [ ((modm .|. shiftMask, xK_c), kill)
 
-    -- launch a terminal
-    [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
+  , ((modm, xK_space ), sendMessage NextLayout)
 
-    -- close focused window
-    , ((modm .|. shiftMask, xK_c), kill)
+  , ((modm, xK_j), windows W.focusDown)
+  , ((modm, xK_k), windows W.focusUp)
 
-     -- Rotate through the available layout algorithms
-    , ((modm, xK_space ), sendMessage NextLayout)
+  , ((modm, xK_Return), windows W.swapMaster)
+  , ((modm .|. shiftMask, xK_j), windows W.swapDown)
+  , ((modm .|. shiftMask, xK_k), windows W.swapUp)
 
-    -- Move focus to the next window
-    , ((modm, xK_j), windows W.focusDown)
+  , ((modm, xK_h), sendMessage Shrink)
+  , ((modm, xK_l), sendMessage Expand)
+  , ((modm, xK_u), sendMessage MirrorShrink)
+  , ((modm, xK_i), sendMessage MirrorExpand)
 
-    -- Move focus to the previous window
-    , ((modm, xK_k), windows W.focusUp  )
+  , ((modm, xK_t), withFocused $ windows . W.sink)
 
-    -- Swap the focused window and the master window
-    , ((modm, xK_Return), windows W.swapMaster)
+  , ((modm .|. shiftMask, xK_h), sendMessage (IncMasterN 1))
+  , ((modm .|. shiftMask, xK_l), sendMessage (IncMasterN (-1)))
 
-    -- Swap the focused window with the next window
-    , ((modm .|. shiftMask, xK_j), windows W.swapDown  )
+  , ((modm .|. controlMask , xK_j), spawn "amixer -q set Master 5- unmute")
+  , ((modm .|. controlMask , xK_k), spawn "amixer -q set Master 5+ unmute")
+  , ((modm .|. controlMask , xK_m), spawn "amixer set Master toggle")
 
-    -- Swap the focused window with the previous window
-    , ((modm .|. shiftMask, xK_k), windows W.swapUp    )
+  , ((modm, xK_c), sendMessage ToggleStruts)
 
-    -- Shrink the master area
-    , ((modm, xK_h), sendMessage Shrink)
+  , ((modm, xK_q), spawn "xmonad --recompile; xmonad --restart")
+  , ((0, xK_Insert), pasteSelection)
 
-    -- Expand the master area
-    , ((modm, xK_l), sendMessage Expand)
-
-    -- Shrink a window
-    , ((modm, xK_u), sendMessage MirrorShrink)
-
-    -- Expand a window
-    , ((modm, xK_i), sendMessage MirrorExpand)
-
-    -- Push window back into tiling
-    , ((modm, xK_t), withFocused $ windows . W.sink)
-
-    -- Increment the number of windows in the master area
-    , ((modm .|. shiftMask, xK_h), sendMessage (IncMasterN 1))
-
-    -- Deincrement the number of windows in the master area
-    , ((modm .|. shiftMask, xK_l), sendMessage (IncMasterN (-1)))
-
-    -- Volume
-    , ((modm .|. controlMask , xK_j), spawn "amixer -q set Master 5- unmute")
-    , ((modm .|. controlMask , xK_k), spawn "amixer -q set Master 5+ unmute")
-    , ((modm .|. controlMask , xK_m), spawn "amixer set Master toggle")
-
-    -- Cover the status bar gap
-    , ((modm, xK_c), sendMessage ToggleStruts)
-
-    -- Programs
-    , ((modm, xK_p), spawn "xfce4-appfinder")
-    , ((modm, xK_o), spawn ("dmenu_run -sb " ++ "\"" ++ myMainColor ++ "\""))
-    , ((modm, xK_b), spawn "chromium")
-    , ((modm, xK_g), spawn "gvim")
-    , ((modm, xK_x), spawn "~/scala-ide/eclipse")
-    , ((modm .|. shiftMask, xK_t), namedScratchpadAction myScratchpads "term")
-    , ((modm .|. shiftMask, xK_n), namedScratchpadAction myScratchpads "keep")
-    , ((modm .|. shiftMask, xK_q), spawn "xfce4-session-logout")
-    , ((modm .|. shiftMask, xK_b), spawn "~/Dropbox/Scala/snapdim/target/start")
-
-    -- Restart xmonad
-    , ((modm, xK_q), spawn "xmonad --recompile; xmonad --restart") ]
-    ++
-
-    --
-    -- mod-[asdf], Switch to workspace N
-    -- mod-shift-[asdf], Move client to workspace N
-    --
-    [((m .|. modm, k), windows $ f i)
-        | (i, k) <- zip (XMonad.workspaces conf) [xK_a, xK_s, xK_d, xK_f, xK_z]
-        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
-    ++
-
-    --
-    -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
-    -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
-    --
-    [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
-        | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
-        , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
-
+  -- Programs
+  , ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
+  , ((modm, xK_p), spawn dmenuCmd)
+  , ((modm, xK_b), spawn "chromium")
+  , ((modm, xK_g), spawn "gvim")
+  , ((modm, xK_x), spawn "~/scala-ide/eclipse")
+  , ((modm .|. shiftMask, xK_t), namedScratchpadAction myScratchpads "term")
+  , ((modm .|. shiftMask, xK_n), namedScratchpadAction myScratchpads "keep")
+  , ((modm .|. shiftMask, xK_b), spawn "~/Dropbox/Scala/snapdim/target/start") ]
+    ++ workspaceKeys ++ monitorKeys
+  where
+    dmenuCmd = "dmenu_run"
+      ++ " -nb \"" ++ (background scheme) ++ "\""
+      ++ " -nf \"" ++ (foreground scheme) ++ "\""
+      ++ " -sf \"" ++ (highlight scheme) ++ "\""
+    workspaceKeys =
+      [((m .|. modm, k), windows $ f i)
+          | (i, k) <- zip (XMonad.workspaces conf) [xK_a, xK_s, xK_d, xK_f, xK_z]
+          , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+    monitorKeys =
+      [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
+          | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
+          , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
 
 -- Mouse bindings: default actions bound to mouse events
 -------------------------------------------------------------------------------
+myMouseBindings :: XConfig l -> M.Map (KeyMask, Button) (Window -> X ())
 myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList
-
-    -- mod-button1, Set the window to floating mode and move by dragging
-    [ ((modm, button1), (\w -> focus w >> mouseMoveWindow w
-       >> windows W.shiftMaster))
-
-    -- mod-button2, Raise the window to the top of the stack
-    , ((modm, button2), (\w -> focus w >> windows W.shiftMaster))
-
-    -- mod-button3, Set the window to floating mode and resize by dragging
-    , ((modm, button3), (\w -> focus w >> mouseResizeWindow w
-       >> windows W.shiftMaster)) ]
+  [ ((modm, button1), (\w -> focus w >> mouseMoveWindow w >> windows W.shiftMaster))
+  , ((modm, button2), (\w -> focus w >> windows W.shiftMaster))
+  , ((modm, button3), (\w -> focus w >> mouseResizeWindow w >> windows W.shiftMaster)) ]
 
 -- Layouts
 ------------------------------------------------------------------------
@@ -145,11 +116,8 @@ myLayout = smartBorders $ spacing 1 $ avoidStruts $ tiled ||| max
 -- > xprop | grep WM_CLASS
 -------------------------------------------------------------------------------
 myManageHook = manageDocks <+> composeAll
-    [ isFullscreen --> doFullFloat
-    , className =? "Xfce4-notifyd" --> doIgnore
-    , className =? "Conky" --> doIgnore
-    , className =? "Xfce4-appfinder" --> doFloat ] 
-    <+> namedScratchpadManageHook myScratchpads
+    [ isFullscreen --> doFullFloat ] 
+      <+> namedScratchpadManageHook myScratchpads
 
 -- Scratchpads
 -------------------------------------------------------------------------------
@@ -169,37 +137,41 @@ myScratchpads = [ NS "term" spawnTerm findTerm managePad
 
 -- Status bars and logging
 -------------------------------------------------------------------------------
+addPad :: String -> String
 addPad = wrap " " " "
 
+colorizer :: (ColorScheme -> String) -> (String -> String)
+colorizer getter = xmobarColor (getter scheme) (background scheme)
+
+myPP :: Handle -> PP
 myPP statusPipe = namedScratchpadFilterOutWorkspacePP xmobarPP 
   { ppOutput = hPutStrLn statusPipe
-  , ppCurrent = xmobarColor myMainColor myBgColor . addPad
-  , ppHiddenNoWindows = xmobarColor myLowColor "" . addPad
-  , ppHidden = xmobarColor myTextcolor "" . addPad
-  , ppTitle = xmobarColor myTextcolor ""
-  , ppSep = xmobarColor myMainColor myBgColor "  |  " }
+  , ppCurrent = colorizer highlight . addPad
+  , ppHiddenNoWindows = colorizer empty . addPad
+  , ppHidden = colorizer hidden . addPad
+  , ppTitle = colorizer foreground
+  , ppSep = (colorizer seperator) "  |  " }
 
 -- Run xmonad with the settings specified. No need to modify this.
 -------------------------------------------------------------------------------
+main :: IO ()
 main = do
-    bar <- spawnPipe "xmobar ~/.xmonad/xmobar.hs"
-    xmonad $ ewmh defaultConfig 
-      { terminal           = "termite"
-      , focusFollowsMouse  = True
-      , borderWidth        = 0
-      , modMask            = mod4Mask
-      , workspaces         = ["A", "S", "D", "F", "Z"]
-      , normalBorderColor  = myNormalBorderColor
-      , focusedBorderColor = myFocusedBorderColor
+  writeFile "debugpoop" "test this shit"
+  bar <- spawnPipe "xmobar ~/.xmonad/xmobar.hs"
+  xmonad $ ewmh defaultConfig 
+    { terminal           = "termite"
+    , focusFollowsMouse  = True
+    , borderWidth        = 0
+    , modMask            = mod4Mask
+    , workspaces         = ["A", "S", "D", "F", "Z"]
 
-      -- bindings
-      , keys               = myKeys
-      , mouseBindings      = myMouseBindings
+    -- bindings
+    , keys               = myKeys
+    , mouseBindings      = myMouseBindings
 
-      -- hooks, layouts
-      , layoutHook         = myLayout
-      , manageHook         = myManageHook
-      , handleEventHook    = fullscreenEventHook
-      , logHook            = dynamicLogWithPP (myPP bar)
-        >> updatePointer (Relative 0.5 0.5)
-      , startupHook        = setWMName "LG3D" }
+    -- hooks, layouts
+    , layoutHook         = myLayout
+    , manageHook         = myManageHook
+    , handleEventHook    = fullscreenEventHook
+    , logHook            = dynamicLogWithPP (myPP bar) >> updatePointer (Relative 0.5 0.5)
+    , startupHook        = setWMName "LG3D" }
